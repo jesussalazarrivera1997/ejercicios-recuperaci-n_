62e42c2783c3d43bd62c55e6dce9ff5f37fc84c7
jesussalazarrivera1997 <43956424+jesussalazarrivera1997@users.noreply.github.com>
2019-03-26 13:50:55 +0100

    s

--- tema 2/worshit 7/ejercicio3.js
+++ tema 2/worshit 7/ejercicio3.js
@@ -9,23 +9,59 @@ function mapaLocalizacionActual2() {
     // The location actual
     var coordenadas1 = {};
     navigator.geolocation.getCurrentPosition(function (position) {
-        coordenadas1 = { lat: position.coords.latitude, lng: position.coords.longitude};
+        coordenadas1 = { lat: position.coords.latitude, lng: position.coords.longitude };
     });
     // The map, centered 
     var map = new google.maps.Map(document.getElementById('map'), { zoom: 4, center: coordenadas1 });
     // The marker, positioned 
-    var marker = new google.maps.Marker({ position: coordenadas1, map: map });
+
+    marker = new google.maps.Marker({
+        map: map,
+        draggable: true,
+        animation: google.maps.Animation.DROP,
+        position: { position: coordenadas1, map: map }
+    });
+    marker.addListener('click', toggleBounce);
 }
 
 
 
-function refrescarMapa(){
+function refrescarMapa() {
     var coordenadas1 = {};
     navigator.geolocation.getCurrentPosition(function (position) {
-        coordenadas1 = { lat: position.coords.latitude, lng: position.coords.longitude};
+        coordenadas1 = { lat: position.coords.latitude, lng: position.coords.longitude };
     });
-    
-}
 
+}
+function initdMap() {
+    //get api uses
+    var directionsService = new google.maps.DirectionsService;
+    var directionsDisplay = new google.maps.DirectionsRenderer;
+    //waypoints to add
+    var waypts = [{ location: { lat: 41.94, lng: 1.56 }, stopover: true }, { location: { lat: 41.99, lng: 1.53 }, stopover: true }, { location: { lat: 41.98, lng: 1.52 }, stopover: true }];
+
+    //api map
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 6,
+        center: { lat: waypts[0].location.lat, lng: waypts[0].location.lng }
+    });
+    //add map
+    directionsDisplay.setMap(map);
+
+    // set the new
+    //new Array(waypts[0].location.lat,waypts[0].location.lng)
+    directionsService.route({
+        origin: { lat: waypts[0].location.lat, lng: waypts[0].location.lng },//db waypoint start
+        destination: { lat: waypts[0].location.lat, lng: waypts[0].location.lng },//db waypoint end
+        waypoints: waypts,
+        travelMode: google.maps.TravelMode.WALKING
+    }, function (response, status) {
+        if (status === google.maps.DirectionsStatus.OK) {
+            directionsDisplay.setDirections(response);
+        } else {
+            window.alert('Ha fallat la comunicació amb el mapa a causa de: ' + status);
+        }
+    });
+}
 
 setInterval()
